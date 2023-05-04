e6b1255eac4e08c070c2b97de90a129590cfacf0
nimairoy <nimairoy718@gmail.com>
2023-05-04 11:54:02 +0600

    loading spinner added

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,19 +1,33 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
+import { CirclesWithBar } from 'react-loader-spinner';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
     const location = useLocation();
 
-    if(loading){
-        return <h2>Loading.........</h2>
+    if (loading) {
+        return <div className="flex justify-center my-24">
+            <CirclesWithBar
+                height="100"
+                width="100"
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                outerCircleColor=""
+                innerCircleColor=""
+                barColor=""
+                ariaLabel='circles-with-bar-loading'
+            />
+        </div>
     }
 
-    if(user){
+    if (user) {
         return children;
     }
-    return <Navigate to='/login' state={{from: location}} replace></Navigate>
+    return <Navigate to='/login' state={{ from: location }} replace></Navigate>
 };
 
 export default PrivateRoute;
